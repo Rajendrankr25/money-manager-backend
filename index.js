b2d9989966f708e302037246ce4b56bffb0d5846
Rajendrankr25 <rajendrankr25@gmail.com>
2023-01-20 14:00:50 +0530

    packages added

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require("express");
+import express, { request, response } from 'express';
 const app = express();
+import cors from "cors";
+import * as dotenv from 'dotenv';
+dotenv.config()
 
-const PORT = 4000;
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
 app.get("/", function (request, response) {
     response.send(`Hi, The Server is Running ${PORT}`);
 });
+
+
 
 app.listen(PORT, () => console.log(`The server started in: ${PORT} ✨✨`));
