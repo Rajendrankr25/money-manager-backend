940e000bc0d5cda91d77cb93b0c9df041fbbe99b
Rajendrankr25 <rajendrankr25@gmail.com>
2023-01-21 17:05:54 +0530

    updte

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", function (request, response) {
-    response.send(`Hi, The Server is Running🌹🌹🌹`);
+    response.send(`Hi, The Server is Running`);
 });
 
 //Money Manager - Expenses
